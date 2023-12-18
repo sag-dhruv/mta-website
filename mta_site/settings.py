8daeecc22d7edd5a20aa09a05be59d2e187c44d9
D_coder <pateldh1234@gmail.com>
2023-12-18 19:08:37 +0530

    Update settings.py

--- mta_site/settings.py
+++ mta_site/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-s16*8=v95$%i*)09w(^cx(wy+c8*!u8kopj!i+qap78&8qqohi'
+SECRET_KEY = 'django-insecure-s16*8=v95$%i*)09w(^cx(wy+c8*!u8kopj!i+qap78&8qqohi'
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
